ac8b3d0b10efa2e01c6d9a2812b3d6d61e708a13
codernoah404 <inhwancho02@gmail.com>
2023-11-10 18:38:16 +0900

    [조인환] 리액트 연결 초기
    리액트 연결을 초기 단계입니다.

--- bangpago/backend/models.py
+++ bangpago/backend/models.py
@@ -50,7 +50,7 @@ class Keyword(models.Model):
     name = models.CharField(max_length=10)
 
 
-class User(models.Model):
+class Users(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
     level = models.IntegerField()
@@ -62,7 +62,7 @@ class Review(models.Model):
         Theme, on_delete=models.CASCADE, related_name="rev_theme_set"
     )
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="rev_user_set"
+        Users, on_delete=models.CASCADE, related_name="rev_user_set"
     )
     playDate = models.CharField(max_length=15, default="")
     grade = models.IntegerField()
--- bangpago/backend/views.py
+++ bangpago/backend/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .serializers import UserSerializer
+from .models import Users
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    
+    queryset = Users.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
